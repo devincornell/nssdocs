8eec359980702744f86e2b59cf39e82e12b5a1e4
Devin J. Cornell <devinj.cornell@gmail.com>
2019-12-03 15:36:05 -0500

    finished example download script.

--- example_download.py
+++ example_download.py
@@ -1,15 +1,16 @@
 
 import urllib.request
-
-
-base = 'https://raw.githubusercontent.com/devincornell/intro_to_text_analysis/master/duke_workshop/nss/'
-urls = (
-    base+'trump_nss.txt',
-    base+'obama_nss.txt',
-)
-
+def download_nss(baseurl='https://raw.githubusercontent.com/devincornell/nssdocs/master/docs/'):
+	def read_url(url):
+		return urllib.request.urlopen(url).read().decode('utf-8')
+	years = (1987, 1988, 1990, 1991, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2002, 2006, 2010, 2015)
+	ftemp = baseurl+'{}.txt'
+	all_texts = [read_url(ftemp.format(year)) for year in years]
+	return {yr:text for yr,text in zip(years,all_texts)}
 
 if __name__ == '__main__':
-	all_texts = [urllib.request.urlopen(url).read().decode('utf-8') for url in urls]
-	print(texts)
+	texts = download_nss()
+	print([(yr,text[:10]) for yr,text in texts.items()])
+
+	
 
